add32dc99c3df932ffdf8062e05cfd48651e7696
jaeyoung <107921434+dd-jero@users.noreply.github.com>
2024-06-30 18:08:42 +0900

    Title: 황금비율 토스트, Time: 24ms, Memory: 3MB, Status: Passed - CodeTree

--- 240630/황금비율 토스트/golden-toast.cpp
+++ 240630/황금비율 토스트/golden-toast.cpp
@@ -12,8 +12,11 @@ int main() {
 
     // 리스트 초기화
     list<char> List(InitBread.begin(), InitBread.end());
-    list<char>::iterator it = --List.end(); // 가리키는 위치 빵의 맨 마지막
+    // 빵의 개수가 T개면, 레시피가 가리킬 수 있는 위치는 총 T+1 곳
+    list<char>::iterator it = List.end(); 
 
+
+    // iterator를 뒤로 움직이기 ++, 앞으로 움직이기 --
     for(int i=0;i<M;i++){
         cin >> cmd;
 
@@ -23,21 +26,22 @@ int main() {
                 it--;
             }
         }else if(cmd == 'R'){
-            if(it != --List.end()){
+            if(it != List.end()){
                 it++;
             }
         }else if(cmd == 'D'){
-            if(it != --List.end()){
-                List.erase(it);
+            if(it != List.end()){
+                // erase는 it에 해당 원소 제거 후, 제거 이후 위치 반환
+                it = List.erase(it);
             }
         }else{
             cin >> tmp;
             List.insert(it, tmp);
-            it++;
         }
     }
 
     for(it = List.begin(); it != List.end(); it++){
+        // iterator는 포인터와 같다. 해당 주소에 담겨있는 데이터 참조
         cout << *it;
     }
 
